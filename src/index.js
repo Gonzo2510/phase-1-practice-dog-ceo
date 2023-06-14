23c830e8595e4eb9d4baa2fd08283e1efb56bc12
jacobgonzales <jagonz@live.com>
2023-06-13 22:16:59 -0500

    added first challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick');
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+
+fetch(imgUrl)
+.then(response => response.json())
+.then(data => {
+    //console.log(data.message)
+    data.message.forEach(dogUrl => {
+        const img = document.createElement('img')
+        img.src = dogUrl
+        const dogImageContainer =  document.getElementById("dog-image-container")
+        dogImageContainer.appendChild(img)
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.querySelector
+// });
