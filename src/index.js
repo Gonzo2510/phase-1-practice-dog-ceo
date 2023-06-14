dda46c8f0c31c22e75aa0e8d7c70868d7544b09d
jacobgonzales <jagonz@live.com>
2023-06-14 13:56:30 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,22 +1,67 @@
 //console.log('%c HI', 'color: firebrick');
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-
-
-fetch(imgUrl)
-.then(response => response.json())
-.then(data => {
-    //console.log(data.message)
-    data.message.forEach(dogUrl => {
-        const img = document.createElement('img')
-        img.src = dogUrl
-        const dogImageContainer =  document.getElementById("dog-image-container")
-        dogImageContainer.appendChild(img)
-    });
+document.addEventListener("DOMContentLoaded", () => {
+    fetchDogs()
+    fetchDogBreeds()
+    filterBreeds()
 });
 
 
 
+//challenge 1
+function fetchDogs() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+        data.message.forEach(dogUrl => {
+            const img = document.createElement('img')
+            img.src = dogUrl
+            const dogImageContainer =  document.getElementById("dog-image-container")
+            dogImageContainer.appendChild(img)
+        });
+    });
+};
+
+//challenge 2
+function fetchDogBreeds() {
+    const breedUrl = "https://dog.ceo/api/breeds/list/all"; 
+
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(data => {
+        const breedKeys = Object.keys(data.message)
+        breedKeys.forEach(breed => {
+            const dogBreeds = document.getElementById("dog-breeds")
+            const li = document.createElement('li')
+            li.textContent = breed
+            dogBreeds.appendChild(li)
+
+            //challenge 3
+            li.addEventListener('click', () =>{
+                li.style.color = 'blue'
+            });
+        });
+    });
+};
+
+//challenge 4
+function filterBreeds() {
+    let dropdown = document.getElementById('breed-dropdown')
+    dropdown.addEventListener('change', () => {
+        const dogBreedsContainer = document.getElementById("dog-breeds")
+        const breeds = dogBreedsContainer.getElementsByTagName('li')
+        const selectedValue = dropdown.value;
+        for (let i = 0; i < breeds.length; i++){
+            if (breeds[i].innerText.startsWith(selectedValue)) {
+                breeds[i].style.display = ''
+            } else {
+                breeds[i].style.display = 'none'
+            }
+        }
+    })
+};
 
 
 
@@ -32,6 +77,4 @@ fetch(imgUrl)
 
 
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     document.querySelector
-// });
+
